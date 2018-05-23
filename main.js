72bee60ef0c0bdb84d607e25785a0b21246e6ae9
Chase Morris <camorris@callutheran.edu>
2018-05-22 21:03:38 -0700

    added new function

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-  const panels = document.querySelectorAll('.panel')
+	const panels = document.querySelectorAll('.panel')
 
-  function toggleOpen(){
-    this.classList.toggle('open')
-  }
+	function toggleOpen(){
+		this.classList.toggle('open')
+	}
 
 function toggleActive(e){
 console.log(e.propertyName)
 if(e.propertyName.includes('flex')){
-  this.classList.toggle('open-active');
+	this.classList.toggle('open-active');
 }
 }
 panels.forEach(panel => panel.addEventListener('click', toggleOpen))
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
+
+function toggleActive(e){
+console.log(e.propertyName)
+if(e.propertyName.includes('flex')){
+	this.classList.toggle('open-active');
+}
+}
+panels.f
